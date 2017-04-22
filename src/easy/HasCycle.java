26b743d2bd86432f1ba8d0825c592931a30863b7
Yinan Zhu <zynsteve@tamu.edu>
2017-04-22 15:17:50 -0500

    Linked List Cycle

---
+++ src/easy/HasCycle.java
@@ -0,0 +1,25 @@
+package easy;
+
+//141
+class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}
+}
+
+public class HasCycle {
+	public boolean hasCycle(ListNode head) {
+		if(head == null) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) return true;
+        }
+        return false;
+    }
+}
