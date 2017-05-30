c29230f8e94b74a9fd9d67a619a04bc513251cb9
Yinan Zhu <zynsteve@tamu.edu>
2017-05-30 16:29:36 -0400

    Palindrome Number

---
+++ src/easy/PalindromeNumber.java
@@ -0,0 +1,22 @@
+package easy;
+
+//9
+public class PalindromeNumber {
+	
+	public boolean isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) return false;
+        int rev = 0;
+        while (x > rev){
+        	rev = rev * 10 + x % 10;
+        	x = x / 10;
+        }
+        return (x == rev || x == rev / 10);
+    }
+	
+	public static void main(String[] arg) {
+		int num = 1234321;
+		PalindromeNumber test = new PalindromeNumber();
+		System.out.println(test.isPalindrome(num));
+	}
+	
+}
