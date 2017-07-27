c443f7d1abdb7989452e8be269db487c3775cade
Yinan Zhu <zynsteve@tamu.edu>
2017-07-27 15:56:51 +0800

    Clone Graph

---
+++ src/medium/CloneGraph.java
@@ -0,0 +1,61 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+//133
+class UndirectedGraphNode {
+	int label;
+    ArrayList<UndirectedGraphNode> neighbors;
+    UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+}
+
+public class CloneGraph {
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if (node == null) {
+            return node;
+        }
+
+        // use bfs algorithm to traverse the graph and get all nodes.
+        ArrayList<UndirectedGraphNode> nodes = getNodes(node);
+        
+        // copy nodes, store the old->new mapping information in a hash map
+        HashMap<UndirectedGraphNode, UndirectedGraphNode> mapping = new HashMap<>();
+        for (UndirectedGraphNode n : nodes) {
+            mapping.put(n, new UndirectedGraphNode(n.label));
+        }
+        
+        // copy neighbors(edges)
+        for (UndirectedGraphNode n : nodes) {
+            UndirectedGraphNode newNode = mapping.get(n);
+            for (UndirectedGraphNode neighbor : n.neighbors) {
+                UndirectedGraphNode newNeighbor = mapping.get(neighbor);
+                newNode.neighbors.add(newNeighbor);
+            }
+        }
+        
+        return mapping.get(node);
+    }
+    
+    private ArrayList<UndirectedGraphNode> getNodes(UndirectedGraphNode node) {
+        Queue<UndirectedGraphNode> queue = new LinkedList<UndirectedGraphNode>();
+        HashSet<UndirectedGraphNode> set = new HashSet<>();
+        
+        queue.offer(node);
+        set.add(node);
+        while (!queue.isEmpty()) {
+            UndirectedGraphNode head = queue.poll();
+            for (UndirectedGraphNode neighbor : head.neighbors) {
+                if(!set.contains(neighbor)){
+                    set.add(neighbor);
+                    queue.offer(neighbor);
+                }
+            }
+        }
+        
+        return new ArrayList<UndirectedGraphNode>(set);
+    }
+}
