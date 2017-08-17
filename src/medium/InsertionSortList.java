708c4afc3b21c9ce3d3d408818859fa157faa256
Yinan Zhu <zynsteve@tamu.edu>
2017-08-17 15:39:41 -0400

    Insertion Sort List

---
+++ src/medium/InsertionSortList.java
@@ -0,0 +1,25 @@
+package medium;
+
+//147
+public class InsertionSortList {
+    /**
+     * @param head: The first node of linked list.
+     * @return: The head of linked list.
+     */
+    public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode(0);
+
+        while (head != null) {
+            ListNode node = dummy;
+            while (node.next != null && node.next.val < head.val) {
+                node = node.next;
+            }
+            ListNode temp = head.next;
+            head.next = node.next;
+            node.next = head;
+            head = temp;
+        }
+
+        return dummy.next;
+    }
+}
